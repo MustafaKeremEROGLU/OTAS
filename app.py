e8dfe6abf96aef380ec14e7f6afd93b62d67b466
kuXst <keremeroglu.44@gmail.com>
2025-08-04 14:00:11 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+# Doğru kullanıcı bilgileri (örnek)
+DOGRU_AD = "admin"
+DOGRU_SIFRE = "1234"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        ad = request.form.get("ad")
+        sifre = request.form.get("sifre")
+        print("Ad:", ad)
+        print("Şifre:", sifre)
+
+        if ad == DOGRU_AD and sifre == DOGRU_SIFRE:
+            return redirect(url_for("admin"))
+        else:
+            return render_template("index.html", hata="Hatalı kullanıcı adı veya şifre!")
+
+    return render_template("index.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+@app.route("/sinif1")
+def sinif1():
+    return render_template("sinif1.html")
+
+@app.route("/sinif2")
+def sinif2():
+    return render_template("sinif2.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
